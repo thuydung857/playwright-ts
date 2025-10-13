11029895fa2da359c09330871a5a10c6deed056d
meomew_auto <hoangbui.ktmt@gmail.com>
2025-10-13 22:51:12 +0700

    chua bai 2

--- tests/homework/bai2.spec.ts
+++ tests/homework/bai2.spec.ts
@@ -12,12 +12,15 @@ test('Kiem tra message va su dung auto wait va assertion', async ({ page }) => {
   await page.getByRole('link', { name: 'Bài 1: Auto-Wait Demo' }).click();
   await page.getByRole('button', { name: 'Nâng cao' }).click();
   await page.getByText('Tôi đồng ý với các điều khoản').click();
-
+  //chỉ cần check đã xác thực là đã thỏa mãn click thành công vào tôi đồng ý
   await expect(page.locator('#terms-status')).toContainText('Đang xác thực...');
   await expect(page.locator('#terms-status')).toContainText('Đã xác thực.');
+  
+  //sai ở đoạn này vì getByRole đang trả ra 3 button -> test fail
   await expect(page.getByRole('button')).toBeEnabled();
 
   await page.getByRole('button', { name: 'Tiếp tục' }).click();
   await expect(page.locator('#spinner')).toContainText('Đang tải dữ liệu người dùng...');
+  //check text cuối cùng là đủ vì playwright tự động waiting ko cần check đang tải dữ liệu
   await expect(page.locator('#final-step')).toContainText('Chào mừng bạn đã quay trở lại!');
 });
