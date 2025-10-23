4e1d4e87a38c3389d5b2c3d052e4997d79392a03
meomew_auto <hoangbui.ktmt@gmail.com>
2025-10-23 22:25:03 +0700

    update bai 4

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,19 +23,15 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 15000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    actionTimeout: 10000,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  expect: {
-    timeout: 6000,
-  },
+
   // expect:{
   //   timeout: 10000
   // },
