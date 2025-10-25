d2f6a0ac707b9a837c1caac07e7a14f0ccde646b
meomew_auto <hoangbui.ktmt@gmail.com>
2025-10-25 15:52:07 +0700

    add lint

--- tests/bai2.spec.ts
+++ tests/bai2.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import {performance} from 'node:perf_hooks'
+import { performance } from 'node:perf_hooks';
 //test scenario (minh se nhom cac test case cung muc dich vao 1 nhom)
 //test suite de nhom cac test() co lien quan lai voi nhua
 // test('vao trang playwright.dev', async ({ page }) => {
@@ -17,8 +17,6 @@ import {performance} from 'node:perf_hooks'
 //   await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
 // });
 
-
-
 //tags
 
 // test.describe('Trang chu playwright.dev', () => {
@@ -33,7 +31,6 @@ import {performance} from 'node:perf_hooks'
 //         await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright')
 //     });
 
-
 // })
 // https://hrm.anhtester.com/erp/desk
 // test.describe('Trang nhan su anh tester', () => {
@@ -46,7 +43,7 @@ import {performance} from 'node:perf_hooks'
 //             await page.getByRole('textbox', { name: 'Enter Password' }).click();
 //             await page.getByRole('textbox', { name: 'Enter Password' }).fill('123456');
 //             await page.getByRole('button', { name: ' Login' }).click();
-//        }) 
+//        })
 //        //
 //        await test.step('Buoc 2: Kiem tra dang nhap thanh cong', async()=>{
 
@@ -56,7 +53,6 @@ import {performance} from 'node:perf_hooks'
 //        })
 //     });
 
-
 // })
 
 //  waitUntil?: "load"|"domcontentloaded"|"networkidle"|"commit";
@@ -65,16 +61,15 @@ import {performance} from 'node:perf_hooks'
 //load: (default) : cho cho toan HTML. CSS, va cac tai nguyen duoc tai xong
 // domcontentloaded: chi cho cho den khi tai lieu HTML ban duoc dc tai va ko can cho CSS hay hinh anh
 //vao trang web A => no yeu cau tai 10 api de co the hien thi duoc danh sach , noi dung trang web
-//list danh sach cac san pham 
-// neu la server side -> be tu dong render -> tra cho client 1 html chua day du thong tin san pham 
-// neu la client side -> no yeu cau minh -> call api get danh sach san pham 
+//list danh sach cac san pham
+// neu la server side -> be tu dong render -> tra cho client 1 html chua day du thong tin san pham
+// neu la client side -> no yeu cau minh -> call api get danh sach san pham
 // networkidle ...... cho cho den khi khong co hoat dong nao dien ra trong vong 500ms phu hop voi site client side rendering.....
-//commit: chi cho cho den khi nhan duoc phan hoi tu server va trinh duyet an dau render trang 
+//commit: chi cho cho den khi nhan duoc phan hoi tu server va trinh duyet an dau render trang
 //trigger (client nhan biet duoc khi nao se bat duoc tai trang)
 
 //trang web noi bo no 1 cai nut la tai trang => khi an vao nut tai trang => call API -> server  xy va phan hoi -> nhan duoc commit
 // -> trang web moi duoc render -> coi nhu hoan thanh
-
 
 // const TARGET_URL = 'https://playwright.dev'
 
@@ -102,12 +97,11 @@ import {performance} from 'node:perf_hooks'
 //     const endTime = performance.now()
 
 //     console.log(`Thoi gian Hoan tat TC02, ${endTime - startTime}`);
-    
+
 //     const searchButton = page.getByRole('button', { name: 'Search (Ctrl+K)' })
 //     await expect(searchButton).toBeEnabled()
-  
-// });
 
+// });
 
 // test('TC03. Demo networkidle', async ({ page }) => {
 //     console.log('DEMO WAIT UNTIL TC03');
@@ -119,18 +113,17 @@ import {performance} from 'node:perf_hooks'
 
 //     console.log(`Thoi gian Hoan tat TC03, ${endTime - startTime}`);
 
-    
-//     //tai thoi diem nay trang da tinh va hoan toan san sang 
+//     //tai thoi diem nay trang da tinh va hoan toan san sang
 //     const searchButton = page.getByRole('button', { name: 'Search (Ctrl+K)' })
 //     await expect(searchButton).toBeEnabled()
-  
+
 // });
-test('test', async ({ page }) => {
-  await page.goto('https://demoapp-sable-gamma.vercel.app/');
-  await page.getByRole('link', { name: 'Bài 1: Auto-Wait Demo' }).click()
-    //default playwright la tan 30s
-//   const button = page.getByRole('button', { name: 'Click Me!' })
-//   await button.click()
-    //const wait locator status.getText() => Button Clicked Successfully!
-  await expect(page.locator('#status')).toContainText('Button Clicked Successfully!');
-});
+// test('test', async ({ page }) => {
+//   await page.goto('https://demoapp-sable-gamma.vercel.app/');
+//   await page.getByRole('link', { name: 'Bài 1: Auto-Wait Demo' }).click();
+//   //default playwright la tan 30s
+//   //   const button = page.getByRole('button', { name: 'Click Me!' })
+//   //   await button.click()
+//   //const wait locator status.getText() => Button Clicked Successfully!
+//   await expect(page.locator('#status')).toContainText('Button Clicked Successfully!');
+// });
